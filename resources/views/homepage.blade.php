01da4fcdbb1922c93d0fe2a3b25dd96f8befc54a
Esentai <esentaik@gmail.com>
2022-11-07 19:02:22 +0600

    07.11.2022

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,13 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <title>Terekalmaz</title>
 </head>
 <body>
+
+
     <header>
         <div class="container">
+
             <nav>
                 <div class="logo">
                     <a href="/">
@@ -25,6 +31,7 @@
                     <a class="nav_link" href="#">Контакты</a>
                 </div>
             </nav>
+
             <div class="header-content">
                 <div class="">
                     <p class="header-text">Представляем</p>
@@ -42,34 +49,34 @@
                     и бурового оборудования
                 </h2>
                 <div class="row">
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item1">
                             <h3 class="assortment_item_name">Алмазные инструменты</h3>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item2">
                             <h3 class="assortment_item_name">Буровые трубы</h3>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item3">
                             <h3 class="assortment_item_name">Буровые установки
                             </h3>
                         </div>
                     </div>
 
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item4">
                             <h3 class="assortment_item_name">Компрессоры</h3>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item5">
                             <h3 class="assortment_item_name">Буровые насосы</h3>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-sm-6 col-xs-6 col-6">
                         <div class="assortment_item assortment_item6">
                             <h3 class="assortment_item_name">Инклинометр
                             </h3>
@@ -86,9 +93,9 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="producer_item">
-                        <div>
-                            <img class="producer_logo" src="{{asset('assets/images/producer_logo.png')}}" alt="">
-                            <div class="producer_item_content">
+                        <div id="rrrr" onmouseover="hoverEffect(this)" onmouseout="normalEffect(this)">
+                            <img  class="producer_logo"  src="{{asset('assets/images/producer_logo.png')}}" alt="">
+                            <div id="producer_item_content" class="producer_item_content">
                                 <p>Терский завод алмазного инструмента (АО «Терекалмаз») – одно из крупнейших российских предприятий, имеющее 50-летний позитивный опыт в производстве алмазного инструмента для бурения, машиностроения и камнеобработки. Неоднократный лауреат всероссийского конкурса «100 лучших товаров России» и признанный лидер в своей отрасли.</p>
                                 <div>
                                     <img src="{{asset('assets/images/producer_img1.png')}}" alt="">
@@ -187,5 +194,14 @@
             <p class="footer_copyright">© 2022 Алматы Все права защищены</p>
         </div>
     </footer>
+
+    <script>
+        function hoverEffect(){
+            document.getElementById('producer_item_content').style.display = 'block'
+        }
+        function normalEffect(){
+            document.getElementById('producer_item_content').style.display = 'none'
+        }
+    </script>
 </body>
 </html>
